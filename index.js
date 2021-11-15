eec00efc26d945983c3e25e3124967e744599cc2
AlejandroMrosas <alejandromrosas@outlook.com>
2021-11-15 00:21:07 -0700

    selections

--- index.js
+++ index.js
@@ -1,43 +1,83 @@
-const fs = require('fs');
+const fs = require('fs'); 
 const inquirer = require('inquirer');
 
-const manager = require('./lib/manager');
-const engineer = require('./lib/engineer');
-const intern = require('./lib/intern');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern'); 
 
 
-function managersprompt() {
-    return inquirer.prompt([
+const employees = []; 
+
+const managerPrompt = () => {
+    return inquirer.prompt ([
         {
             type: 'list',
             message: 'Select Member role.',
-            choices: ['Manager', 'engineer', 'intern']
+            choices: ['Manager', 'Engineer', 'Intern']
         }, {
             type: 'input',
             message: 'Who is your manager?',
-            name: 'Managername'
+            name: 'managerName'
         }, {
             type: 'input',
             message: 'Manager ID?',
-            name: 'ManagerID'
+            name: 'managerId'
         }, {
             type: 'input',
             message: 'Manager Email?',
-            name: 'ManagerEmail'
+            name: 'managerEmail'
         }, {
             type: 'input',
             message: 'Manager Github username?',
             name: 'github'
         }, 
     ])
-
-.then((response) => {
-    return fs.writeFile('/', err => {
-        if (err) {
-            console.log(err)
-            return
-            }
-        console.log("Successful");
-    });
-  });
 };
+
+const employeePrompt = () => {
+    return inquirer.prompt ([
+        {
+            type: 'list',
+            name: 'role',
+            message: "Please choose your employee's role",
+            choices: ['Engineer', 'Intern']
+        },
+        {
+            type: 'input',
+            name: 'employeeName',
+            message: "What's the name of the employee?", 
+        },
+        {
+            type: 'input',
+            name: 'employeeId',
+            message: "Enter Employee's ID",
+        },
+        {
+            type: 'input',
+            name: 'employeeEmail',
+            message: "Enter the employee's email.",
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: "Enter github username.",
+        },
+        {
+            type: 'confirm',
+            name: 'AddNewEmployee',
+            message: 'Would you like to add more team members?',
+        }
+    ])
+
+};
+
+const writeFile = data => {
+    fs.writeFile('./dist/index.html', data, err => {
+        if (err) {
+            console.log(err);
+            return;
+        } else {
+            console.log("Profile created");
+        }
+    })
+}; 
